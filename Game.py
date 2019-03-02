8706eb7b3aaa31bf2590c891713fc4cb8c366b4a
Peter Feng <pfengdev@gmail.com>
2019-03-02 15:58:33 -0800

    Refactoring game.py to be a class with a run function

--- Game.py
+++ Game.py
@@ -5,61 +5,76 @@ from SnakePiece import SnakePiece
 from SpawnFood import SpawnFood
 from GridInfo import GridInfo
 
-WIDTH = 800
-HEIGHT = 800
-ROW_NUM = HEIGHT/SnakePiece.SIZE
-COL_NUM = WIDTH/SnakePiece.SIZE
-gameOver = False
+class Game:
+	WIDTH = 800
+	HEIGHT = 800
+	ROW_NUM = HEIGHT/SnakePiece.SIZE
+	COL_NUM = WIDTH/SnakePiece.SIZE
+	gameOver = False
 
-pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.init()
+	window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-background = pygame.Surface(window.get_size())
-background.fill((0,0,0))
+	background = pygame.Surface(window.get_size())
+	background.fill((0,0,0))
 
-gridInfo = GridInfo((WIDTH, HEIGHT, ROW_NUM, COL_NUM))
-snake = Snake()
-spawnFood = SpawnFood(gridInfo)
-snakePieces = []
-food = []
-running = True
+	gridInfo = GridInfo((WIDTH, HEIGHT, ROW_NUM, COL_NUM))
+	snake = Snake()
+	spawnFood = SpawnFood(gridInfo)
+	snakePieces = []
+	food = []
+	running = True
 
-def checkRunning(events):
-	for event in events:
-		if event.type == KEYDOWN:
-			if event.key == K_ESCAPE:
-				return False
-		elif event.type == QUIT:
-				return False
-	return True
+	def checkRunning(self, events):
+		for event in events:
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					return False
+			elif event.type == QUIT:
+					return False
+		return True
 
-def render():
-	window.blit(background, (0, 0))
-	for snakePiece in snake.getSnakePieces():
-		window.blit(snakePiece.surf, snakePiece.rect)
-	for f in food:
-		window.blit(f.surf, f.rect)
-	pygame.display.flip()
+	def render(self):
+		self.window.blit(self.background, (0, 0))
+		for snakePiece in self.snake.getSnakePieces():
+			self.window.blit(snakePiece.surf, snakePiece.rect)
+		for f in self.food:
+			self.window.blit(f.surf, f.rect)
+		pygame.display.flip()
 
-def updateSnake():
-	snake.move()
+	def updateSnake(self):
+		self.snake.move()
 
-def update():
-	updateSnake()
+	def update(self):
+		self.updateSnake()
 
-def handleInput(events):
-	snake.handleInput(events)
+	def handleInput(self, events):
+		self.snake.handleInput(events)
 
-while running:
-	render()
-	events = pygame.event.get()
-	running = checkRunning(events)
-	handleInput(events)
-	update()
-	if not food:
-		food.append(spawnFood.spawn(snake.getSnakePieces(), gridInfo))
-	pygame.time.wait(5)
+	def shouldSpawnFood(self):
+		return not self.food
 
+	def spawnFood(self):
+		if self.shouldSpawnFood():
+			self.food.append(self.spawnFood.spawn(self.snake.getSnakePieces(), self.gridInfo))
+
+	def run(self):
+		while self.running:
+			self.render()
+			events = pygame.event.get()
+			self.running = self.checkRunning(events)
+			self.handleInput(events)
+			self.update()
+			pygame.time.wait(5)
+
+game = Game()
+game.run()
+
+# self.checkSnakeEatFood()
+# def checkSnakeEatFood(self):
+# 	head = self.snake.getSnakePieces[0];
+# 	if head.rect.x == food[0].rect.x and head.rect.y == food[0].rect.y:
+# 		self.snake.eat(food)
 
 # gameLoop() {
 # 	while(!gameOver) {
@@ -73,14 +88,6 @@ while running:
 # 		}
 # 	}
 # 	displayGameOver();
-# }
-
-# checkSnakeEatFood() {
-# 	head = positions[0];
-# 	if head.equals(food.position) {
-# 		snake.eat(food);
-# 		shouldSpawnFood = true;
-# 	}
 # }
 
 # checkSnakeDie() {
